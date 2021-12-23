d31be1bdea793189c9e70ad40c4505d3e919c886
Ferdinand Rios <frios@viciouscircle.com>
2021-12-23 11:02:25 -0800

    Initial Commit

---
+++ Tests/StarRatingTests/StarRatingTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import StarRating
+
+final class StarRatingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(StarRating().text, "Hello, World!")
+    }
+}
